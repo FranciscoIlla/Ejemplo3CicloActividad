b3b49269b53e60df75053f005c7c27a4c0343480
franciscoillacases <francisco.illacases@cieep.com>
2023-09-18 11:24:48 +0200

    Initial commit

---
+++ app/src/main/java/francisco/illa/ejemplo3cicloactividad/SecondActivity.java
@@ -0,0 +1,14 @@
+package francisco.illa.ejemplo3cicloactividad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
